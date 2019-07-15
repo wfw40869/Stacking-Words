2674583afc2c783876cd3ab22db8501ebe0fc685
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-07-15 14:08:18 -0500

    Added html form and functionality to handle user inputted sentence, punctuations and number of punctuations

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,38 @@
-let sentenceArray = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+let sentenceArray = [];
+const sentence = document.querySelector("#sentence"),
+    punctuation = document.querySelector("#punctuation"),
+    numberOfPunctuations = document.querySelector("#numberOfPunctuations"),
+    submitButton = document.querySelector("#submitButton"),
+    printHere = document.querySelector(".printHere")
 
 
-const addExcitement = (wordsArray, punctuation, amountOfPunctuations) => {
-    let buildMeUp = ""
-    let counter = 1
+
+const addExcitementAndCreateElements = (wordsArray, punctuation, amountOfPunctuations) => {
+    // let buildMeUp = ""
+    let counter = 1.
+    buildMeUp = ""
     for (let index = 0; index < wordsArray.length; index++) {
+        let paragraph = document.createElement("p")
         if (counter % 3 === 0) {
-            buildMeUp += `${wordsArray[index]}${punctuation.repeat(amountOfPunctuations)} `;
+            buildMeUp += `${wordsArray[index]}${punctuation.repeat(amountOfPunctuations)} `
+            paragraph.innerHTML = buildMeUp
+            printHere.appendChild(paragraph)
         } else {
-            buildMeUp += `${wordsArray[index]} `;
-            console.log(buildMeUp);
+            buildMeUp += `${wordsArray[index]} `
+            paragraph.innerHTML = buildMeUp
+            printHere.appendChild(paragraph)
         }
         counter++
     }
 }
 
-addExcitement(sentenceArray, "?", 10);
+
+
+
+submitButton.addEventListener("click", (event) => {
+    sentenceArray = sentence.value.split(" ")
+    addExcitementAndCreateElements(sentenceArray, punctuation.value, parseInt(numberOfPunctuations.value))
+    // Prevent default form submit behavior
+    event.preventDefault();
+})
 
